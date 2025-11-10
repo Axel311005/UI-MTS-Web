7ee902accef464a0bbb8e02433f649a105e62595
Axel Pérez <axeleliasperezrodriguez2@gmail.com>
2025-11-10 11:14:50 -0600

    asd

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,28 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import path from 'path';
-import { visualizer } from 'rollup-plugin-visualizer';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     visualizer({
-      filename: 'dist/stats.html',
+      filename: "dist/stats.html",
       gzipSize: true,
       brotliSize: true,
     }),
   ],
   esbuild: {
-    drop: ['console', 'debugger'],
+    drop: ["console", "debugger"],
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   css: {
-    postcss: './postcss.config.js',
+    postcss: "./postcss.config.js",
   },
   build: {
     // Configuración básica de build sin chunks manuales
